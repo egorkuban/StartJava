977e7f53484be8c1b3bddb9120014a6c062ed03a
egorkuban <t469mo@gmail.com>
2020-04-29 13:09:43 +0300

    Code_Variable

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+  public static void main(String[] args) {
+    byte numberCPU = 2;
+    Short RamMb = 8192;
+    int graphicsCardMsiMb = 2048;
+    long hardDiskMemoryKb = 1_038_090_240L;
+    float cpuChz = 3.4f;
+    double processorSpeed = 2.07;
+    char motherBoard = 'M';
+    boolean notebook = false;
+    String cpu = "Intel (R) Core (TM) i3-3240";
+    System.out.println("Number CPU" + numberCPU);
+    System.out.println("RAM Mb" + RamMb);
+    System.out.println("graphics card Mb" + graphicsCardMsiMb);
+    System.out.println("The average processor speed" + processorSpeed);
+    System.out.println("CPU frequency" + cpuChz);
+    System.out.println("Hard Disk Memory Kb" + hardDiskMemoryKb);
+    System.out.println("MSI mother Board" + motherBoard);
+    System.out.println("notebook" + notebook);
+    System.out.println("The processor name"+cpu);
+  }
+
+}
